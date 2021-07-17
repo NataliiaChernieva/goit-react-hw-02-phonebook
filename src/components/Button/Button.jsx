99905182e9352eb1f2fcc2625a7a9069073b900e
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-07-17 14:36:14 +0300

    готовая работа

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { CustomButton } from "./Button.styled";
 
-function Button({ text, type }) {
-  return <CustomButton type={type}>{text}</CustomButton>;
+function Button({ text, type, onClick }) {
+  return <CustomButton type={type} onClick={onClick}>{text}</CustomButton>;
 }
 
-Button.PropTypes = {
-  text: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-}
+// Button.PropTypes = {
+//   text: PropTypes.string.isRequired,
+//   type: PropTypes.string.isRequired,
+// }
 
 export default Button;
